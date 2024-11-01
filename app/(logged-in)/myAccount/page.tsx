885234ed0077ed8551a16b3a22b6d1e064dc1a48
Ani <a.gvin3@gmail.com>
2024-11-01 22:17:01 +0400

    c

--- app/(logged-in)/myAccount/page.tsx
+++ app/(logged-in)/myAccount/page.tsx
@@ -1,3 +1,19 @@
-export default function MyAccount() {
-  return <div>MyAccount</div>;
+import { auth } from "@/auth";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+
+export default async function MyAccount() {
+const session = await auth()
+
+  return (
+ 
+    <Card className="w-[350px]">
+        <CardHeader>My Accounnt</CardHeader>
+        <CardContent>
+            <Label>Email Address</Label>
+            <div className="text-muted-foreground">{session?.user?.email}</div>
+        </CardContent>
+    </Card>
+
+  );
 }
